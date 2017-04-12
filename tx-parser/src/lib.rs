1e63f715de13ff1a25cd8201097bf824cb4adb9b
Brian Grinstead <briangrinstead@gmail.com>
2017-04-12 16:48:56 -0700

    initial benchmarks (in file)

--- tx-parser/src/lib.rs
+++ tx-parser/src/lib.rs
@@ -9,6 +9,7 @@
 // specific language governing permissions and limitations under the License.
 
 #![allow(dead_code)]
+#![feature(test)]
 
 extern crate combine;
 #[macro_use]
@@ -19,6 +20,8 @@ extern crate mentat_tx;
 
 #[macro_use]
 extern crate mentat_parser_utils;
+
+extern crate test;
 
 use combine::{
     eof,
@@ -182,6 +185,7 @@ pub fn remove_db_id(map: &mut MapNotation) -> std::result::Result<Option<EntidOr
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test::Bencher;
 
     use std::collections::BTreeMap;
 
@@ -301,5 +305,52 @@ mod tests {
         let result = parser.parse(input.with_spans().into_atom_stream()).map(|x| x.0);
         assert_eq!(result,
                    Ok(Entity::MapNotation(expected)));
+    }
+
+    #[bench]
+    fn bench_parse1(b: &mut Bencher) {
+        let input = r#"
+            [[:db/add 101 :test/a "v"]
+             [:db/add "tempid" :test/a "v"]
+             [:db/retract 102 :test/b "w"]]"#;
+        let parsed_edn = edn::parse::value(input).expect("to parse test input");
+        b.iter(|| {
+            return Tx::parse(parsed_edn.clone());
+        });
+    }
+
+    #[bench]
+    fn bench_parse2(b: &mut Bencher) {
+        let input = r#"
+            [[:db/add 1  :test/val "a"]
+             [:db/add 2  :test/val "b"]
+             [:db/add 3  :test/val "c"]
+             [:db/add 4  :test/val "d"]
+             [:db/add 5  :test/val "e"]
+             [:db/add 6  :test/val "f"]
+             [:db/add 7  :test/val "g"]
+             [:db/add 8  :test/val "h"]
+             [:db/add 9  :test/val "i"]
+             [:db/add 10 :test/val "j"]
+             [:db/add 11 :test/val "k"]
+             [:db/add 12 :test/val "l"]
+             [:db/add 13 :test/val "m"]
+             [:db/add 14 :test/val "n"]
+             [:db/add 15 :test/val "o"]
+             [:db/add 16 :test/val "p"]
+             [:db/add 17 :test/val "q"]
+             [:db/add 18 :test/val "r"]
+             [:db/add 19 :test/val "s"]
+             [:db/add 20 :test/val "t"]
+             [:db/add 21 :test/val "u"]
+             [:db/add 22 :test/val "v"]
+             [:db/add 23 :test/val "w"]
+             [:db/add 24 :test/val "x"]
+             [:db/add 25 :test/val "y"]
+             [:db/add 26 :test/val "z"]]"#;
+        let parsed_edn = edn::parse::value(input).expect("to parse test input");
+        b.iter(|| {
+            return Tx::parse(parsed_edn.clone());
+        });
     }
 }
